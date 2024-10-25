18340b9c9ade37a42506b596b5077de6b9be4e75
Lindokuhle Zwane <ST10381088@rcconnect.edu.za>
2024-10-25 14:36:12 +0200

    Added Submitted Claims Controller

--- CMCSWebApp/Controllers/SubmittedClaimsController.cs
+++ CMCSWebApp/Controllers/SubmittedClaimsController.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using CMCSWebApp.Data;
+using CMCSWebApp.Interfaces;
+using CMCSWebApp.Models;
+using CMCSWebApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCSWebApp.Controllers
+{
+    public class SubmittedClaimsController : Controller
+    {
+        private readonly ISubmittedClaimsRepository _submittedClaimsRepository;
+
+        public SubmittedClaimsController(ISubmittedClaimsRepository submittedClaimsRepository)
+        {
+            _submittedClaimsRepository = submittedClaimsRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // bring table from the database / execute SQL
+            IEnumerable<SubmittedClaims> sbClaims = await _submittedClaimsRepository.GetAllClaimsAsync();
+
+            // give you all the claims
+            return View(sbClaims);
+        }
+
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+    }
+}
