8bfaffc3ffeb5af9ee31650a57a96086cd96878c
Lindokuhle Zwane <ST10381088@rcconnect.edu.za>
2024-10-25 14:39:11 +0200

    ClaimFormController : Controller IClaimFormRepository _claimFormRepository;

--- CMCSWebApp/Controllers/ClaimFormController.cs
+++ CMCSWebApp/Controllers/ClaimFormController.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using CMCSWebApp.Data;
+using CMCSWebApp.Interfaces;
+using CMCSWebApp.Models;
+using CMCSWebApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCSWebApp.Controllers
+{
+    public class ClaimFormController : Controller
+    {
+
+        private readonly IClaimFormRepository _claimFormRepository;
+
+        public ClaimFormController(IClaimFormRepository claimFormRepository)
+        {
+            _claimFormRepository = claimFormRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
