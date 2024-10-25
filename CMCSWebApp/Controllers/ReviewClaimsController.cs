12c1b83b7985b44429fa3819a62d3822431dd216
Lindokuhle Zwane <ST10381088@rcconnect.edu.za>
2024-10-25 14:36:59 +0200

    Added Review Claims Controller

--- CMCSWebApp/Controllers/ReviewClaimsController.cs
+++ CMCSWebApp/Controllers/ReviewClaimsController.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using CMCSWebApp.Data;
+using CMCSWebApp.Interfaces;
+using CMCSWebApp.Models;
+using CMCSWebApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMCSWebApp.Controllers
+{
+    public class ReviewClaimsController : Controller
+    {
+        private readonly IReviewClaimsRepository _reviewClaimsRepository;
+
+        public ReviewClaimsController(IReviewClaimsRepository reviewClaimsRepository)
+        {
+            _reviewClaimsRepository = reviewClaimsRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // bring table from the database / execute SQL
+            IEnumerable<ReviewClaims> rvClaims = await _reviewClaimsRepository.GetAllClaimsAsync();
+
+            // give you all the claims
+            return View(rvClaims);
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
