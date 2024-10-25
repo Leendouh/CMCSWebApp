e9b34d37cae5567133ec2008a4102db8e4adf8b1
Lindokuhle Zwane <ST10381088@rcconnect.edu.za>
2024-10-25 14:38:38 +0200

    ClaimsController : Controller for the IClaimRepository claimRepository

--- CMCSWebApp/Controllers/ClaimsController.cs
+++ CMCSWebApp/Controllers/ClaimsController.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using CMCSWebApp.Data;
+using CMCSWebApp.Interfaces;
+using CMCSWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCSWebApp.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private readonly IClaimRepository _claimRepository;
+
+        public ClaimsController(IClaimRepository claimRepository)
+        {
+            _claimRepository = claimRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // bring table from the database / execute SQL
+            IEnumerable<Claims> claims = await _claimRepository.GetAllClaimsAsync();
+
+            // give you all the claims
+            return View(claims);
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
